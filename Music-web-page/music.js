9879279d9d734d543d0f27b53cae75ae264a1aad
EvannLu <evan.lu60@myhunter.cuny.edu>
2024-01-10 16:31:46 -0500

    update w/ daniel

--- Music-web-page/music.js
+++ Music-web-page/music.js
@@ -31,7 +31,8 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-function submitSongs() {
+async function submitSongs() { //run in its own time
+
   // Retrieve values from input boxes
   var song1 = document.getElementById('song1').value;
   var song2 = document.getElementById('song2').value;
@@ -45,4 +46,32 @@ function submitSongs() {
   console.log('Song 4:', song4);
   console.log('Song 5:', song5);
 
+  const response = await fetch('http://localhost:8000/songs', {
+    method: "POST", body: JSON.stringify({
+      songNames: [song1, song2, song3, song4, song5],
+      
+    })
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not OK');  // Handle non-200 status codes
+    }
+    return response.json();  // Parse the response as JSON
+  })
+
+
+}
+
+async function refreshSongs() {
+  console.log('hi')
+
+
+  const response = await fetch('http://localhost:8000/votes', {
+    method: "GET"
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not OK');  // Handle non-200 status codes
+    }
+    return response.json();  // Parse the response as JSON
+  })
+  console.log(response)
 }
