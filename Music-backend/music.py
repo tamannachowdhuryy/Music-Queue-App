a00365afd6d63e7d3c5e32b46b7452fe8bfc13e1
Tamanna Chowdhury <tamanna.chowdhury610@gmail.com>
2024-01-10 22:32:35 -0500

    updates

--- Music-backend/music.py
+++ Music-backend/music.py
@@ -1,1 +1,64 @@
+import http.server
+import socketserver
+from http import HTTPStatus
+import json
 
+
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    preferences = {
+
+    }
+
+
+    def do_POST(self):
+        # Set the required headers for the POST request.
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+
+        # Read the body of the request. This is the "body" we set in the Javascript code.
+        content_length = int(self.headers["Content-Length"])
+        body = self.rfile.read(content_length)
+        body_message = json.loads(body)
+
+        # Do some processing logic. This is the "meat and potatoes" of your app.
+        print("Recieved Request: ")
+        print(body_message)
+        if (self.path == '/songs'): 
+            print('/songs')
+            for song in body_message['songNames']:
+                if song == '':
+                    continue
+                if song in self.preferences:
+                    self.preferences[song] = self.preferences[song] + 1
+                else:
+                    self.preferences[song] = 1
+                
+            # self.preferences += (body_message['songNames']) 
+            print(self.preferences)
+            self.wfile.write(json.dumps({}).encode())
+        elif (self.path == '/votes'):
+            print('vote')
+            
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+        print(self.path)
+
+        if(self.path == '/votes'):
+            print('hi')
+            self.wfile.write(json.dumps({'songNames' : self.preferences}).encode())
+            return 
+        self.wfile.write(json.dumps({}).encode())
+        print('hi2')
+
+
+port_num = 8000
+httpd = socketserver.TCPServer(("", port_num), Handler)
+print("Starting Up on Port " + str(port_num))
+httpd.serve_forever()
--- Music-web-page/music.js
+++ Music-web-page/music.js
@@ -1,48 +1,77 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let header = document.querySelector('header');
-    let sections = document.querySelectorAll('section');
-    let navLinks = document.querySelectorAll('nav a');
-  
-    window.addEventListener('scroll', function() {
-      let fromTop = window.scrollY;
-  
-      // Add fixed class to header when scrolling down
-      if (fromTop > 50) {
-        header.classList.add('fixed');
-      } else {
-        header.classList.remove('fixed');
+  let header = document.querySelector('header');
+  let sections = document.querySelectorAll('section');
+  let navLinks = document.querySelectorAll('nav a');
+
+  window.addEventListener('scroll', function() {
+    let fromTop = window.scrollY;
+
+    // Add fixed class to header when scrolling down
+    if (fromTop > 50) {
+      header.classList.add('fixed');
+    } else {
+      header.classList.remove('fixed');
+    }
+
+    // Highlight the current section in the navigation
+    sections.forEach(sec => {
+      let top = sec.offsetTop - 50;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+
+      if (fromTop > top && fromTop < top + height) {
+        navLinks.forEach(link => {
+          link.classList.remove('active');
+        });
+
+        document.querySelector('nav a[href*="' + id + '"]').classList.add('active');
       }
-  
-      // Highlight the current section in the navigation
-      sections.forEach(sec => {
-        let top = sec.offsetTop - 50;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-  
-        if (fromTop > top && fromTop < top + height) {
-          navLinks.forEach(link => {
-            link.classList.remove('active');
-          });
-  
-          document.querySelector('nav a[href*="' + id + '"]').classList.add('active');
-        }
-      });
     });
   });
-  
-  
-  function submitSongs() {
-    // Retrieve values from input boxes
-    var song1 = document.getElementById('song1').value;
-    var song2 = document.getElementById('song2').value;
-    var song3 = document.getElementById('song3').value;
-    var song4 = document.getElementById('song4').value;
-    var song5 = document.getElementById('song5').value;
-  
-    console.log('Song 1:', song1);
-    console.log('Song 2:', song2);
-    console.log('Song 3:', song3);
-    console.log('Song 4:', song4);
-    console.log('Song 5:', song5);
-  
-  }
+});
+
+
+async function submitSongs() { //run in its own time
+
+  // Retrieve values from input boxes
+  var song1 = document.getElementById('song1').value;
+  var song2 = document.getElementById('song2').value;
+  var song3 = document.getElementById('song3').value;
+  var song4 = document.getElementById('song4').value;
+  var song5 = document.getElementById('song5').value;
+
+  console.log('Song 1:', song1);
+  console.log('Song 2:', song2);
+  console.log('Song 3:', song3);
+  console.log('Song 4:', song4);
+  console.log('Song 5:', song5);
+
+  const response = await fetch('http://localhost:8000/songs', {
+    method: "POST", body: JSON.stringify({
+      songNames: [song1, song2, song3, song4, song5],
+      
+    })
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not OK');  // Handle non-200 status codes
+    }
+    return response.json();  // Parse the response as JSON
+  })
+
+
+}
+
+async function refreshSongs() {
+  console.log('hi')
+
+
+  const response = await fetch('http://localhost:8000/votes', {
+    method: "GET"
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not OK');  // Handle non-200 status codes
+    }
+    return response.json();  // Parse the response as JSON
+  })
+  console.log(response)
+}
